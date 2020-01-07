7fd683e45e0ffc087c3bc6f79785c3d525a7dd43
Logstor <a.rottger_rydahl@live.dk>
2020-01-07 23:27:54 +0100

    Settings.java  - Now sets cheat button correctly at entrance

--- app/src/main/java/com/example/galgeleg/fragments/settings/SettingsFragment.java
+++ app/src/main/java/com/example/galgeleg/fragments/settings/SettingsFragment.java
@@ -11,18 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.galgeleg.R;
+import com.example.galgeleg.model.settings.Settings;
 
 public class SettingsFragment extends Fragment implements View.OnClickListener
 {
-	private View view;
-	
+	// Fields
+	private Settings settings;
 	private Switch btnSwitch;
 	
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
 	{
-		return view = inflater.inflate(R.layout.settings_fragment, container, false);
+		return inflater.inflate(R.layout.settings_fragment, container, false);
 	}
 	
 	@Override
@@ -30,19 +31,31 @@ public class SettingsFragment extends Fragment implements View.OnClickListener
 	{
 		super.onViewCreated(view, savedInstanceState);
 		
+		// Get the settings instance
+		settings = Settings.getInstance();
+		
 		// Get view references
 		btnSwitch = view.findViewById(R.id.switchBar);
 		
 		// Set OnClick listeners
 		btnSwitch.setOnClickListener(this);
+		
+		setBtnSwitch();
 	}
 	
 	@Override
 	public void onClick(View v)
 	{
 		if (v == btnSwitch)
-		{
-		
-		}
+		{ settings.switchCheats(); }
+	}
+	
+	/**
+	 * Takes care of setting the slider to the correct state
+	 * depending on what the setting is.
+	 */
+	private void setBtnSwitch()
+	{
+		if (settings.isCheatEnabled()) btnSwitch.setChecked(true);
 	}
 }
